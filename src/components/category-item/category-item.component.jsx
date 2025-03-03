2ff6dc5874df56073269b4d90dc7f45f8f465276
Mike <mike.j.parish@gmail.com>
2025-03-03 15:33:41 -0500

    routes addes

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -2,6 +2,7 @@ import "./category-item.styles.scss";
 
 const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+
   return (
     <div className="category-container">
       <div
