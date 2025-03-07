f43acef2333dba68db4b8eccf5702cbba8d17108
Mike <mike.j.parish@gmail.com>
2025-03-07 10:34:04 -0500

    102 custom button

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import FormInput from "../form-input/form-input.component.jsx";
+import Button from "../button/button.component.jsx";
+import "./sign-up-form.styles.scss";
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -48,8 +50,9 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput
           label="Display Name"
@@ -87,7 +90,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
