35403caa62fe2e670692367a472f732d2baeb0c3
Mike <mike.j.parish@gmail.com>
2025-03-13 14:26:59 -0400

    122 creating checkout page

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,33 @@
+import { UserContext } from '../../contexts/user.context';
+
+import { CartContext } from '../../contexts/cart.context';
+
+import './checkout.styles.scss';
+import { useContext } from 'react';
+
+const Checkout = ()=>{
+
+  const {cartItems, addItemToCart} = useContext(CartContext);
+
+  return (
+    <div>
+      <h1>I am the checkout page</h1>
+      <div>
+        {
+          cartItems.map((cartItem)=>{
+            const {id, name, quantity} = cartItem
+            return( 
+              <div key={id}>
+                <h2>{name}</h2>
+                <span>decrement </span>
+                <span>{quantity}</span>
+                <span onClick={()=>addItemToCart(cartItem)}> increment</span>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  )
+}
+
+export default Checkout;
