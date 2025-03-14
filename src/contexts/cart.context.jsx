0d3541fc67c1e9e46ea2e653a98e99b9da89cf3e
Mike <mike.j.parish@gmail.com>
2025-03-14 15:33:26 -0400

    125 checkout item pt3

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -32,12 +32,15 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
   );
 } 
 
+const clearCartItem = (cartItems, cartItemToClear) => cartItems.filter(carItem => carItem.id !== cartItemToClear.id)
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: ()=>{},
   cartItems: [],
   addItemToCart: ()=>{},
   removeItemFromCart: ()=>{},
+  clearItemFromCart: ()=>{},
   cartItemCount: 0,
 });
 
@@ -61,11 +64,16 @@ export const CartProvider = ({children}) =>{
     setCartItems(removeCartItem(cartItems, cartItemToRemove));
   }
 
+  const clearItemFromCart = (cartItemToClear)=>{
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+  }
+
   const value = {
       isCartOpen,
       setIsCartOpen, 
       addItemToCart,
       removeItemFromCart,
+      clearItemFromCart,
       cartItems, 
       cartItemCount
   };
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -9,7 +9,7 @@ import './checkout.styles.scss';
 
 const Checkout = ()=>{
 
-  const {cartItems, addItemToCart, removeItemFromCart} = useContext(CartContext);
+  const {cartItems} = useContext(CartContext);
 
   return (
     <div className='checkout-container'>
