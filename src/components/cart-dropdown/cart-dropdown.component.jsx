3d8583339ed72ef38373e18705db520e3701b1d8
Mike <mike.j.parish@gmail.com>
2025-03-19 15:07:40 -0400

    139 sytled-component cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,7 @@ import { CartContext } from '../../contexts/cart.context.jsx';
 import Button from '../button/button.component.jsx';
 import CartItem from '../cart-item/cart-item.component.jsx';
 
-import './cart-dropdown.styles.scss';
+import {CartDropdownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles.jsx';
 
 const CartDropdown = ()=>{
 
@@ -18,14 +18,16 @@ const CartDropdown = ()=>{
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems.map((item)=> (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item)=> <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>Checkout</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
