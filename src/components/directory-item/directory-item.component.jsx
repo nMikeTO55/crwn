ec6aeea323ba03da9774378bc46106755805a20b
Mike <mike.j.parish@gmail.com>
2025-03-20 11:34:34 -0400

    142 last touches

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,20 @@
-import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles.jsx";
+import { useNavigate } from "react-router-dom";
+
+import { 
+  BackgroundImage, 
+  Body, 
+  DirectoryItemContainer 
+} from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageurl, title, route } = category;
+  const navigate = useNavigate(route);
+
+  const onNavigateHandler = () => navigate(route);
 
   return (
-    <DirectoryItemContainer>
-      <BackgroundImage imageUrl={imageUrl} />
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageurl={imageurl} />
       <Body>
         <h2>{title}</h2>
         <p>Shop Now</p>
